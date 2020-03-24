e82b137f6aaa3e8b43be3fcb8a6eb971ec1be805
hesammoosapour <hesammoosapour@yahoo.com>
2020-03-24 20:52:10 +0430

    finally webpack node modules and auth and resources -bootstrap are working properly for first time as first project

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,19 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
