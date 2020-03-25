b79e9836d94b2c8c2702740156daef4443be9e26
hesammoosapour <hesammoosapour@yahoo.com>
2020-03-25 21:49:40 +0430

    right now i m about to change libs.css and bring another one from codehacking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+{{--    <link href="{{asset('css/libs.css')}}" rel="stylesheet">--}}
 
 </head>
 <body>
@@ -28,7 +29,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand title_clock" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img class=" clock_nav_img" height="40" src="/images/Clock.jpg" alt="Clock">
+                    {{ config('app.name') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
