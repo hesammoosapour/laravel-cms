823255bd3fed69442b894099ea764c4311c6f038
hesammoosapour <hesammoosapour@yahoo.com>
2020-03-26 10:01:27 +0430

    Not adjusted well I had to comment old codes of libs.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-{{--    <link href="{{asset('css/libs.css')}}" rel="stylesheet">--}}
+    <link href="{{asset('css/libs.css')}}" rel="stylesheet">
 
 </head>
 <body>
